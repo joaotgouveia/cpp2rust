c2472ee5b14d6692ee28cfd099ec3c15b0bdd34b
João Gouveia <joaotalonegouveia@tecnico.ulisboa.pt>
2026-06-25 18:20:08 +0000

    use parent dir name for uid

--- cpp2rust/converter/translation_rule.cpp
+++ cpp2rust/converter/translation_rule.cpp
@@ -394,7 +394,7 @@ std::pair<ExprRules, TypeRules> LoadPartial(const std::filesystem::path &dir) {
   auto *root = parsed->getAsObject();
   assert(root);
 
-  const auto parent = std::filesystem::absolute(dir).string();
+  const auto parent = std::filesystem::absolute(dir).filename().string();
   for (auto &[entry_name, entry_val] : *root) {
     auto name = entry_name.str();
     auto val = entry_val.getAsString();
