16b2164ce5f1b5501855c61f257ba553de2d8d9f
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-09-01 15:53:33 +0100

    Enable experimental library

--- rules/lib/rule_hints.h
+++ rules/lib/rule_hints.h
@@ -432,7 +432,8 @@ DECLARE_HINT(MbState) : private Role<>, public std::mbstate_t {
   template <int Id> PROBE_ONLY operator MbState<Id>() const;
 };
 
-#if defined(__GLIBCXX__)
+#if defined(__cpp_lib_execution) ||                                            \
+    (defined(_LIBCPP_HAS_EXPERIMENTAL_PSTL) && _LIBCPP_HAS_EXPERIMENTAL_PSTL)
 DECLARE_HINT(ExecutionPolicy) : private Role<>,
                                 public std::execution::parallel_policy {
   template <int Id> PROBE_ONLY operator ExecutionPolicy<Id>() const;
@@ -1177,6 +1178,7 @@ struct is_error_condition_enum<ErrorConditionEnum<N>> : true_type {};
 #define FormatContext FormatContext<__COUNTER__>
 #endif
 #define MbState MbState<__COUNTER__>
-#if defined(__GLIBCXX__)
+#if defined(__cpp_lib_execution) ||                                            \
+    (defined(_LIBCPP_HAS_EXPERIMENTAL_PSTL) && _LIBCPP_HAS_EXPERIMENTAL_PSTL)
 #define ExecutionPolicy ExecutionPolicy<__COUNTER__>
 #endif
